a67f4a1d0cc87ea909f954a0ff6f1fa78eafcdb6
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-18 19:07:23 -0600

    InjectRepository

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+import { Product } from './entities/product.entity';
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productReposytory: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all products`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} product`;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  async update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
+  async emove(id: number) {
     return `This action removes a #${id} product`;
   }
 }
