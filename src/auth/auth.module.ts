2ce223f6003bb15bcb5716992abedca69735ca12
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-22 01:05:26 -0600

    JwtModule.registerAsync

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    //! registrar modulo asyncrono
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('SECRET_KEY'),
+          signOptions: {
+            expiresIn: '30d',
+          },
+        };
+      },
+    }),
+    //! registrar modulo no asyncrono
+    // JwtModule.register({
+    //   secret: process.env.SECRET_KEY,
+    //   signOptions: {
+    //     expiresIn: '30d',
+    //   },
+    // }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [TypeOrmModule],
