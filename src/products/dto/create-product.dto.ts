289fe69b2c3b0215014560125384a5d77a4964e6
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-23 13:14:02 -0600

    @ApiProperty()

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -20,40 +20,48 @@ export class CreateProductDto {
   @MinLength(1)
   title: string;
 
+  @ApiProperty()
   @IsNumber()
   //   @Transform(({ value }) => value.trim())
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty()
   @Transform(({ value }) => value.toLowerCase().trim())
   @IsString()
   @MinLength(1)
   description: string;
 
+  @ApiProperty()
   @Transform(({ value }) => value.toLowerCase().trim())
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty()
   @Transform(({ value }) => value.toLowerCase().trim())
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
